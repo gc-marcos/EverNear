90c27e9bf9713e6eb017e51cbeafbd910e6bd1fc
gcmarcoss <gcmarcoss@gmail.com>
2026-01-30 21:42:32 +0000

    :sparkles: feat(navigation): adiciona navegação da MainActivity para PatientActivity

--- app/src/main/java/com/marcoscarvalho/evernear/MainActivity.java
+++ app/src/main/java/com/marcoscarvalho/evernear/MainActivity.java
@@ -1,6 +1,9 @@
 package com.marcoscarvalho.evernear;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,25 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        ImageButton patientButton = findViewById(R.id.btn_patient);
+        ImageButton caregiverButton = findViewById(R.id.btn_caregiver);
+
+        patientButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        caregiverButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
