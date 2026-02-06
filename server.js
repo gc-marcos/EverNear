042a302a1c78fece8d1aa56c68af002b756af1fb
gcmarcos <54316695-gcmarcos@users.noreply.replit.com>
2026-02-06 17:56:22 +0000

    ✨ feat(web-preview): cria preview web da aplicação

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 5000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/assets', express.static(path.join(__dirname, 'app/src/main/res')));
+
+app.use((req, res, next) => {
+  res.set('Cache-Control', 'no-cache, no-store, must-revalidate');
+  res.set('Pragma', 'no-cache');
+  res.set('Expires', '0');
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`EverNear web preview running on http://0.0.0.0:${PORT}`);
+});
